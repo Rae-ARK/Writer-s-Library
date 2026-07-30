7de2c7d37d9f40e4f5768b6b719151cb0f20f1a6
Rae-ARK <horizonarkstudio@gmail.com>
2026-07-30 15:00:46 +0530

    Initial project setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yourname.webnovellibrary',
+  appName: 'Webnovel Library Manager',
+  webDir: 'build'
+};
+
+export default config;
